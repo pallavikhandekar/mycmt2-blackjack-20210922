ac651f0b48c42cc748c7054b8609e583ff3a9295
Ted M. Young <tedyoung@gmail.com>
2021-09-22 16:04:17 -0700

    Lab #4: Added StubDeck and first Game Outcome test for player going bust and losing.

--- src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
+++ src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
@@ -1,13 +1,14 @@
 package com.jitterted.ebp.blackjack;
 
 import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleGame;
+import com.jitterted.ebp.blackjack.domain.Deck;
 import com.jitterted.ebp.blackjack.domain.Game;
 
 // Startup, Bootstrap, Assembler
 public class Blackjack {
 
     public static void main(String[] args) {
-        Game game = new Game();
+        Game game = new Game(new Deck());
         ConsoleGame consoleGame = new ConsoleGame(game); // in general: Entities aren't directly passed in to Adapters
         consoleGame.start();
     }
--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
@@ -15,55 +15,39 @@ public class ConsoleGame {
         this.game = game;
     }
 
-    public static void resetScreen() {
-        System.out.println(ansi().reset());
+    public void start() {
+        displayWelcomeScreen();
+
+        game.initialDeal();
+
+        playerPlays();
+
+        game.dealerTurn();
+
+        displayFinalGameState();
+
+        System.out.println(game.determineOutcome());
+
+        resetScreen();
     }
 
-    public static void displayWelcomeScreen() {
-        System.out.println(ansi()
-                                   .bgBright(Ansi.Color.WHITE)
-                                   .eraseScreen()
-                                   .cursor(1, 1)
-                                   .fgGreen().a("Welcome to")
-                                   .fgRed().a(" Jitterted's")
-                                   .fgBlack().a(" BlackJack"));
+    private void playerPlays() {
+        while (!game.isPlayerDone()) {
+            displayGameState();
+            String command = inputFromPlayer();
+            handle(command);
+        }
     }
 
-    public static String inputFromPlayer() {
-        System.out.println("[H]it or [S]tand?");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+    private void handle(String command) {
+        if (command.toLowerCase().startsWith("h")) {
+            game.playerHits();
+        } else if (command.toLowerCase().startsWith("s")) {
+            game.playerStands();
+        }
     }
 
-    public static void displayBackOfCard() {
-        System.out.print(
-                ansi()
-                        .cursorUp(7)
-                        .cursorRight(12)
-                        .a("┌─────────┐").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ J I T ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E R ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E D ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("└─────────┘"));
-    }
-
-    public static void displayGameState(Game game) {
-        System.out.print(ansi().eraseScreen().cursor(1, 1));
-        System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.displayFirstCard(game.dealerHand())); // first card is Face Up
-
-        // second card is the hole card, which is hidden
-        displayBackOfCard();
-
-        System.out.println();
-        System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
-        System.out.println(" (" + game.playerHand().value() + ")");
-    }
-
-    public static void displayFinalGameState(Game game) {
+    private void displayFinalGameState() {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
         System.out.println("Dealer has: ");
         System.out.println(ConsoleHand.cardsAsString(game.dealerHand()));
@@ -75,36 +59,38 @@ public class ConsoleGame {
         System.out.println(" (" + game.playerHand().value() + ")");
     }
 
-    public void start() {
-        displayWelcomeScreen();
+    private void displayGameState() {
+        System.out.print(ansi().eraseScreen().cursor(1, 1));
+        System.out.println("Dealer has: ");
+        System.out.println(ConsoleHand.displayFirstCard(game.dealerHand())); // first card is Face Up
 
-        game.initialDeal();
+        // second card is the hole card, which is hidden
+        ConsoleCard.displayBackOfCard();
 
-        playerPlays();
-
-        game.dealerTurn();
-
-        displayFinalGameState(game);
-
-        System.out.println(game.determineOutcome());
-
-        resetScreen();
+        System.out.println();
+        System.out.println("Player has: ");
+        System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
+        System.out.println(" (" + game.playerHand().value() + ")");
     }
 
-    public void playerPlays() {
-        while (!game.isPlayerDone()) {
-            displayGameState(game);
-            String command = inputFromPlayer();
-            handle(command);
-        }
+    private String inputFromPlayer() {
+        System.out.println("[H]it or [S]tand?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
-    public void handle(String command) {
-        if (command.toLowerCase().startsWith("h")) {
-            game.playerHits();
-        } else if (command.toLowerCase().startsWith("s")) {
-            game.playerStands();
-        }
+    private void resetScreen() {
+        System.out.println(ansi().reset());
+    }
+
+    private void displayWelcomeScreen() {
+        System.out.println(ansi()
+                                   .bgBright(Ansi.Color.WHITE)
+                                   .eraseScreen()
+                                   .cursor(1, 1)
+                                   .fgGreen().a("Welcome to")
+                                   .fgRed().a(" Jitterted's")
+                                   .fgBlack().a(" BlackJack"));
     }
 
 }
