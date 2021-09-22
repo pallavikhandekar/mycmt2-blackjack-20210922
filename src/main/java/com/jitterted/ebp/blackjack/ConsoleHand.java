b0258c70bca05c754afc3706e8857cf7fce56af3
Ted M. Young <tedyoung@gmail.com>
2021-09-22 11:48:28 -0700

    Lab #2: Extracted and moved displaying the full hand, inlining output into Game class.

--- src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
+++ src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
@@ -1,9 +1,20 @@
 package com.jitterted.ebp.blackjack;
 
+import java.util.stream.Collectors;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
 public class ConsoleHand {
 
     // Take a DOMAIN OBJECT and return (translate/transform/map) DTO (in this case a String is good enough)
     static String displayFirstCard(Hand hand) {
         return ConsoleCard.display(hand.firstCard());
+    }
+
+    public static String cardsAsString(Hand hand) {
+        return hand.cards().stream()
+                   .map(ConsoleCard::display)
+                   .collect(Collectors.joining(
+                            ansi().cursorUp(6).cursorRight(1).toString()));
     }
 }
